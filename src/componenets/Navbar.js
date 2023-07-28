781746d98f335946d3d8c90dd5cf66b894af6d2b
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2023-07-28 11:57:21 +0530

    Update Navbar.js

--- src/componenets/Navbar.js
+++ src/componenets/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar() {
                             <img alt="e-dashboard" src="http://w7.pngwing.com/pngs/17/58/png-transparent-web-development-responsive-web-design-e-commerce-business-ecommerce-blue-angle-web-design.png" width="30" height="24"/>
                         </a> 
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0"> 
                                 
                                 <li className="nav-item">
                                     <Link className="nav-link active" aria-current="page" to="/">Products</Link>
