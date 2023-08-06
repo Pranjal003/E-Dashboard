506924d6c890d86be23de24650969138d543ca2f
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2023-08-06 15:10:43 +0530

    Update Footer.js

--- src/componenets/Footer.js
+++ src/componenets/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
   {/* <div className="container pt-4">
      
     <section className="mb-4">
-      
+       
       <a
         className="btn btn-link btn-floating btn-lg text-dark m-1"
         href="/"
