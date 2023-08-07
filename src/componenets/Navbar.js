22084baeb49c726cd7839944e8ad1bf961b2aef0
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2023-08-07 21:31:53 +0530

    Update Navbar.js

--- src/componenets/Navbar.js
+++ src/componenets/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 export default function Navbar() {
-    const auth = localStorage.getItem('user');
+    const auth = localStorage.getItem('user'); 
     const navigate = useNavigate();
     const logout = () => {
         localStorage.clear();
