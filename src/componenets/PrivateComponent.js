912976341c7295d3ad6240d65dfb5d3262e12ffa
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2023-08-13 19:18:05 +0530

    Update PrivateComponent.js

--- src/componenets/PrivateComponent.js
+++ src/componenets/PrivateComponent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
  
-const PrivateComponent = () =>{
+const PrivateComponent = () =>{ 
     const auth  = localStorage.getItem('user');
     return auth?<Outlet />:<Navigate to="/signUp" />
 }
