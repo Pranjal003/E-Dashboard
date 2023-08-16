f3bca65f71f2f560fd6ec4462cd87b3e178062ee
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2023-08-16 20:14:14 +0530

    Update SignUp.js

--- src/componenets/SignUp.js
+++ src/componenets/SignUp.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 export default function SignUp() {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    const [password, setPassword] = useState("") 
     const navigate = useNavigate() 
 
     useEffect(()=>{
