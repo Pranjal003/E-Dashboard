1ae900a65b2c6fb05268f5acc4b045180c2f5137
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2023-08-28 19:32:49 +0530

    Update AddProducts.js

--- src/componenets/AddProducts.js
+++ src/componenets/AddProducts.js
@@ -13,7 +13,7 @@ export default function AddProducts() {
     var forms = document.querySelectorAll('.needs-validation')  
   
     // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
+    Array.prototype.slice.call(forms) 
       .forEach(function (form) {
         form.addEventListener('submit', function (event) {
           if (!form.checkValidity()) {
