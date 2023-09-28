a2b5b32011a35ff1ae65b5e12d51dc2fcc4efc59
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2023-09-28 16:00:40 +0530

    Update Login.js

--- src/componenets/Login.js
+++ src/componenets/Login.js
@@ -5,7 +5,7 @@ export default function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const navigate = useNavigate();
-
+ 
     useEffect(()=>{
         const auth  = localStorage.getItem('user');
         if(auth){
