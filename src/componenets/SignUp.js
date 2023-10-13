e6721f7cd5338f9ab7f5c30fbff2a76cdd680b64
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2023-10-13 22:42:09 +0530

    Update SignUp.js

--- src/componenets/SignUp.js
+++ src/componenets/SignUp.js
@@ -11,7 +11,7 @@ export default function SignUp() {
     useEffect(()=>{
         const auth  = localStorage.getItem('user'); 
         if(auth){ 
-            navigate('/')
+            navigate('/') 
         }
     })  
 
